6116d026ba120b9c0269feee67095b4da923f180
MubashirCephal <mubashirkappan@cephal.ae>
2026-08-21 00:23:21 +0530

    feat: implement multi-stage Docker development configuration with hot-reloading and update Nuxt favicon assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/img/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/img/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '48x48', href: '/img/favicon-48x48.png' },
+        { rel: 'icon', type: 'image/png', sizes: '64x64', href: '/img/favicon-64x64.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/img/favicon-96x96.png' },
+        { rel: 'apple-touch-icon', sizes: '512x512', href: '/img/favicon-512x512.png' },
+      ]
+    }
+  },
   modules: [
     '@nuxt/ui',
     '@nuxtjs/google-fonts',
